8256655e505490f60e98a35609273af87ee6b764
suainul2 <suainulovi@gmail.com>
2022-11-14 09:28:43 +0700

    rename faca

--- src/Facades/Rajaongkir.php
+++ src/Facades/Rajaongkir.php
@@ -4,7 +4,7 @@ namespace Suainul\Rajaongkir\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class RajaongkirFacade extends Facade
+class Rajaongkir extends Facade
 {
     /**
      * Get the registered name of the component.
